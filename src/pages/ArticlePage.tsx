78b84fcb12d8d2811ffd5460f9c8b07b51adeef4
deevarzariu <dell.varzariu@gmail.com>
2023-02-15 23:38:01 +0200

    Created Article page

--- src/pages/ArticlePage.tsx
+++ src/pages/ArticlePage.tsx
@@ -1,5 +1,28 @@
+import { useLocation } from "react-router-dom";
+import style from "assets/styles/ArticlePage.module.scss";
+
 function ArticlePage() {
-  return <div>article</div>;
+  const { state } = useLocation();
+  const date = new Date(state.publishedAt).toDateString();
+
+  return (
+    <div className={style.article}>
+      <h2 className={style.article__title}>{state.title}</h2>
+      <div className={style.article__date}>
+        Published on <span>{date}</span>
+      </div>
+      <div>
+        <img className={style.article__image} src={state.urlToImage} alt="" />
+      </div>
+      <div className={style.article__content}>{state.content}</div>
+      <div className={style.article__link}>
+        Find the original article{" "}
+        <a className={style.article__link__text} href={state.url}>
+          here
+        </a>
+      </div>
+    </div>
+  );
 }
 
 export default ArticlePage;
