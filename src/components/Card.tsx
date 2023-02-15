e2499cbff3d98c1611c30955e8b7381bff207d6d
deevarzariu <dell.varzariu@gmail.com>
2023-02-15 23:39:09 +0200

    Implemented navigation to article

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,18 +1,27 @@
 import { Article } from "../types/Article";
 import style from "../assets/styles/Card.module.scss";
+import { useNavigate } from "react-router-dom";
 
 type Props = {
   article: Article;
 };
 
 function Card({ article }: Props) {
+  const navigate = useNavigate();
+
+  function openArticle() {
+    const id = Math.floor(Math.random() * 10000);
+    navigate(`/article/${id}`, { state: article });
+  }
+
   return (
-    <div className={style.card}>
+    <div className={style.card} onClick={openArticle}>
       <div className={style["card__image-container"]}>
-        <img className={style.card__image} src={article.urlToImage} />
+        <img className={style.card__image} src={article.urlToImage} alt="" />
       </div>
-      <div className="card__text-container">
+      <div className={style["card__text-container"]}>
         <div className={style.card__title}>{article.title}</div>
+        <div className={style.card__description}>{article.description}</div>
       </div>
     </div>
   );
--- src/components/Homepage/HeroStageCard.tsx
+++ src/components/Homepage/HeroStageCard.tsx
@@ -1,3 +1,4 @@
+import { useNavigate } from "react-router-dom";
 import { Article } from "types/Article";
 import style from "assets/styles/HeroStageCard.module.scss";
 
@@ -7,9 +8,16 @@ type Props = {
 };
 
 function HeroStageCard({ article, position }: Props) {
+  const navigate = useNavigate();
+
+  function openArticle() {
+    const id = Math.floor(Math.random() * 10000);
+    navigate(`/article/${id}`, { state: article });
+  }
+
   return (
-    <div className={`${style.card} ${style[position]}`}>
-      <img className={style.image} src={article.urlToImage} />
+    <div className={`${style.card} ${style[position]}`} onClick={openArticle}>
+      <img className={style.image} src={article.urlToImage} alt="" />
       <div className={style.title}>{article.title}</div>
     </div>
   );
