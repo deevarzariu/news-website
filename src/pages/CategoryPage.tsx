984b18f18e4611bd3e888abfa4474c802c3581ee
deevarzariu <dell.varzariu@gmail.com>
2023-02-17 22:58:30 +0200

    Populated category page with data

--- src/pages/CategoryPage.tsx
+++ src/pages/CategoryPage.tsx
@@ -1,5 +1,32 @@
+import Grid from "components/common/Grid";
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { getArticlesByCategory } from "services/News";
+import { Article } from "types/Article";
+
 function CategoryPage() {
-  return <div className="page">category</div>;
+  const [articles, setArticles] = useState<Article[]>([]);
+  const { category = "" } = useParams();
+
+  useEffect(() => {
+    async function getArticles() {
+      try {
+        const headlines = await getArticlesByCategory(category);
+        setArticles(headlines);
+        console.log(headlines);
+      } catch (err) {
+        console.log(err);
+      }
+    }
+
+    getArticles();
+  }, [category]);
+
+  return (
+    <div className="page">
+      <Grid articles={articles} />
+    </div>
+  );
 }
 
 export default CategoryPage;
--- src/services/News.ts
+++ src/services/News.ts
@@ -1,5 +1,4 @@
 import axios from "axios";
-import { Article } from "../types/Article";
 
 const BASE_URL = "https://newsapi.org/v2";
 const API_KEY = process.env.REACT_APP_API_KEY;
@@ -7,6 +6,16 @@ const API_KEY = process.env.REACT_APP_API_KEY;
 export async function getTopHeadlines() {
   const res = await axios.get(
     `${BASE_URL}/top-headlines?country=us&apiKey=${API_KEY}`
+  );
+  const {
+    data: { articles },
+  } = res;
+  return articles;
+}
+
+export async function getArticlesByCategory(category: string) {
+  const res = await axios.get(
+    `${BASE_URL}/top-headlines?country=us&category=${category}&apiKey=${API_KEY}`
   );
   const {
     data: { articles },
